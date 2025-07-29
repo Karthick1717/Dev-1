df2b0fe9f006eed65f2d4fa34fefa673af0f5eb7
Karthick1717 <karthiabd1717l@gmail.com>
2025-07-30 00:08:47 +0530

    update

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -1,5 +1,6 @@
 const Attendance = require("../models/Attendance");
 
+
 exports.upsertDailyAttendance = async (req, res) => {
   const { phone, month, day, status } = req.body;
 
@@ -37,3 +38,69 @@ exports.upsertDailyAttendance = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+
+
+exports.getAllAttendance = async (req, res) => {
+  const phone = req.user.mobile;
+
+
+
+
+  try {
+    const records = await Attendance.find({ phone });
+    console.log(records)
+
+  
+
+    if (!records || records.length === 0) {
+      return res.status(404).json({ message: "No attendance found" });
+    }
+
+    res.status(200).json(records);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getMonthlyAttendance = async (req, res) => {
+const phone = req.user.mobile;
+  const { month } = req.params;
+
+  try {
+    const record = await Attendance.findOne({ phone, month });
+
+    if (!record) {
+      return res.status(404).json({ message: "No record for this month" });
+    }
+
+    res.status(200).json(record);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getDailyStatus = async (req, res) => {
+ const phone = req.user.mobile;
+  const { month, day } = req.params;
+
+  try {
+    const record = await Attendance.findOne({ phone, month });
+
+    if (!record) {
+      return res.status(404).json({ message: "Month record not found" });
+    }
+
+    const dayRecord = record.records.find(r => r.day === day);
+
+    if (!dayRecord) {
+      return res.status(404).json({ message: "No record for this day" });
+    }
+
+    res.status(200).json({ day, status: dayRecord.status });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
